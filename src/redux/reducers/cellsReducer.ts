71b80c67104cbe53ebe46fe0f7ca2d984befec09
Gbenga <gbenga.olufeyimi@venturegardengroup.com>
2021-03-11 23:06:03 +0100

    Worked on INSERT_CELL_BEFORE_REDUCER

--- src/redux/reducers/cellsReducer.ts
+++ src/redux/reducers/cellsReducer.ts
@@ -34,23 +34,40 @@ const cellsReducer = produce(
         state.order[index] = state.order[targetIndex];
         state.order[targetIndex] = action.payload.id;
 
-        return;
+        return state;
       case actionType.DELETE_CELL:
         delete state.data[action.payload];
         state.order = state.order.filter((id) => id !== action.payload);
-        return;
+        return state;
       case actionType.UPDATE_CELL:
         const { id, content } = action.payload;
         state.data[id].content = content;
-        return;
+        return state;
       case actionType.INSERT_CELL_AFTER:
         return { ...state };
       case actionType.INSERT_CELL_BEFORE:
-        return { ...state };
+        const cell: Cell = {
+          content: "",
+          type: action.payload.type,
+          id: randomId(),
+        };
+        state.data[cell.id] = cell;
+        const foundIndex = state.order.findIndex(
+          (id) => id === action.payload.id
+        );
+
+        if (foundIndex < 0) {
+          state.order.push(cell.id);
+        } else {
+        }
+        state.order.splice(foundIndex, 0, cell.id);
+        return state;
       default:
         return { ...state };
     }
   }
 );
+
+const randomId = () => Math.random().toString(36).substring(2, 5);
 
 export default cellsReducer;
